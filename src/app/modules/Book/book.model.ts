c0374acccb15b25509a2819357d000b41fa2319c
Anowar Hosen <anowarhosen444@gmail.com>
2025-06-20 15:56:18 +0600

    feat: remove version key and add timestamps on book model

--- src/app/modules/Book/book.model.ts
+++ src/app/modules/Book/book.model.ts
@@ -2,25 +2,27 @@ import { model, Schema } from "mongoose";
 import { IBook } from "./book.interface";
 
 // Book Schema Using Mongoose
-const bookSchema = new Schema<IBook>({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  genre: {
-    type: String,
-    enum: [
-      "FICTION",
-      "NON_FICTION",
-      "SCIENCE",
-      "HISTORY",
-      "BIOGRAPHY",
-      "FANTASY",
-    ],
+const bookSchema = new Schema<IBook>(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: {
+      type: String,
+      enum: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+    },
+    isbn: { type: String, required: true, unique: true },
+    description: { type: String },
+    copies: { type: Number, min: 0 },
+    available: { type: Boolean, default: true },
   },
-  isbn: { type: String, required: true, unique: true },
-  description: { type: String },
-  copies: { type: Number, min: 0 },
-  available: { type: Boolean, default: true },
-});
-
+  { versionKey: false, timestamps: true }
+);
 
 export const Book = model<IBook>("Book", bookSchema);
