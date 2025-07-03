5cbd19c1ec97bb54c02c60db3e5f788e09bf45fa
Anowar Hosen <anowarhosen444@gmail.com>
2025-07-03 09:58:15 +0600

    fix: modify cors allowed urls

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import cors from "cors";
 import express, { Application, NextFunction, Request, Response } from "express";
-import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import router from "./app/routes";
 
 const app: Application = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://bookland-server.vercel.app"],
+  })
+);
 
 // using routes
 app.use(router);
@@ -17,8 +21,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Library Management Server Running");
 });
 
-
-
 // route error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(404).json({
@@ -27,13 +29,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-
-
 // Global error handler
 app.use(globalErrorHandler);
-
-
-
-
 
 export default app;
